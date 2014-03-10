e581f478cae32f8881b230fe741b156c20e111b7
Fabian <Fabian.Kuerten@rwth-aachen.de>
2014-03-11 00:26:35 +0100

    adapt to redesigned build system

--- config.mk
+++ config.mk
@@ -19,14 +19,9 @@
 
 # Configuration section
 MODULE_NAME=basics
+MODULE_MAJOR_VERSION=1
+MODULE_MINOR_VERSION=0
 DEPENDENCIES=
 LIBS:=-ldl
-CXXFLAGS+= --std=c++11
-ARFLAGS:=crvs
+CXX_STANDARD=c++11
 
-# Automatic section
-INCLUDEDIRS:=${MODULEDIR}/target/generated
-iftrue ${DEPENDENCIES}
-	INCLUDEDIRS+=${MODULEDIR}/../${DEPENDENCIES}/src/main/c++
-endif
-CPPFLAGS+=-I${INCLUDEDIRS}
